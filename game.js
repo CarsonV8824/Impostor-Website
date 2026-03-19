fa5228128caf9d63858d0d434efe099c561458a7
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-03-18 20:44:29 -0500

    done. Add Game loop now

--- game.js
+++ game.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(playerCount)
     let categories = []
     let randomcategory
-
+    let agentWord
+    let impostorWord
+    let impostorIndex
     let gameList = [];
 
     function pickRandomCategory(data) {
@@ -12,37 +14,48 @@ document.addEventListener("DOMContentLoaded", () => {
         return random;
     }
 
+    function initializeGame(categoriesData) {
+        randomcategory = pickRandomCategory(categoriesData)
+        
+        let categoryName = randomcategory.name;
+        let categoryItems = randomcategory.words;
+
+        agentWord = categoryItems[Math.floor(Math.random() * categoryItems.length)]
+        impostorWord = categoryItems[Math.floor(Math.random() * categoryItems.length)]
+
+        while (agentWord === impostorWord) {
+            impostorWord = categoryItems[Math.floor(Math.random() * categoryItems.length)]
+        }
+        console.log(categoryName)
+        console.log(agentWord)
+        console.log(impostorWord)
+        
+        impostorIndex = Math.floor(Math.random() * (playerCount-1))
+        for (let i=0; i<playerCount; i++){
+            if (i === impostorIndex){
+                gameList.push(impostorWord)
+                continue;
+            } else {
+                gameList.push(agentWord)
+            }
+        }
+        console.log(gameList)
+    }
+
     fetch("categories.json")
         .then(response => response.json())
         .then(data => {
             categories = data
             console.log("Now it's loaded:", categories)
+            initializeGame(categories)
+            const infoText = document.getElementById("infoText")
+            const turnBtn = document.getElementById("turnBtn")
 
-            // use categories here
-            randomcategory = pickRandomCategory(categories)
-            
-            let categoryName = randomcategory.name;
-            let categoryItems = randomcategory.words;
+            //  TODO: 
+            turnBtn.addEventListener("click", () => {
+                return;
+            })
 
-            let agentWord = categoryItems[Math.floor(Math.random() * categoryItems.length)]
-            let impostorWord = categoryItems[Math.floor(Math.random() * categoryItems.length)]
-
-            while (agentWord === impostorWord) {
-                impostorWord = categoryItems[Math.floor(Math.random() * categoryItems.length)]
-            }
-            console.log(categoryName)
-            console.log(agentWord)
-            console.log(impostorWord)
-            let impostorIndex = Math.floor(Math.random() * (playerCount-1))
-            for (let i=0; i<playerCount; i++){
-                if (i === impostorIndex){
-                    gameList.push(impostorWord)
-                    continue;
-                } else {
-                    gameList.push(agentWord)
-                }
-            }
-            console.log(gameList)
         })
         .catch(error => console.error("Failed to load JSON file:", error))
     
